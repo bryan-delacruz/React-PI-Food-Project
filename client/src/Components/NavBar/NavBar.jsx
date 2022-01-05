1da8e2e02b1d27aec9a5feb14e92802196c2c6d9
Bryan De La Cruz <bryan.delacruza@gmail.com>
2022-01-05 13:09:36 -0500

    refactor NavBar file

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,32 +1,27 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
+import { getRecipesByName } from "../../redux/actions";
 
 import style from "./NavBar.module.css";
 import find from "./svg/find.svg";
 
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { getRecipesByName } from "../../redux/actions";
-import { useState } from "react";
-
 const NavBar = () => {
+  const filters_and_order = useSelector((state) => state.filters_and_order);
   const dispatch = useDispatch();
   let navigate = useNavigate();
 
-  const filters_and_order = useSelector((state) => state.filters_and_order);
-  const [input, setInput] = useState("");
-
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
-  };
   const handleOnSubmit = (e) => {
     e.preventDefault();
     let obj = {
       ...filters_and_order,
-      byTitle: input,
+      byTitle: e.target.title.value,
     };
     dispatch(getRecipesByName(obj));
     navigate("/home");
   };
+
   return (
     <nav className={style.nav}>
       <div className={style.nav_container}>
@@ -41,8 +36,6 @@ const NavBar = () => {
             autoComplete="off"
             name="title"
             type="text"
-            onChange={(e) => handleInputChange(e)}
-            value={input}
             className={style.nav_input}
             placeholder="Ingresar receta"
           />
