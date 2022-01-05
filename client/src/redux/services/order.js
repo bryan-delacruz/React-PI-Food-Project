f24bbe4b27fc1c7d2ad0f13a500a35b70b00d726
Bryan De La Cruz <bryan.delacruza@gmail.com>
2022-01-05 16:35:08 -0500

    Refactor Sort by HigScore and SmaScore

--- client/src/redux/services/order.js
+++ client/src/redux/services/order.js
@@ -1,5 +1,4 @@
 export const order = (array, byOrder) => {
-
   if (byOrder === "orderAsc") {
     array = array.sort((a, b) => {
       var nameA = a.title.toUpperCase(); // ignore upper and lowercase
@@ -27,9 +26,39 @@ export const order = (array, byOrder) => {
       return 0;
     });
   } else if (byOrder === "orderHigScore") {
-    array = array.sort((b, a) => a.spoonacularScore - b.spoonacularScore);
+    array = array.sort((b, a) => {
+      if (a.spoonacularScore - b.spoonacularScore !== 0) {
+        return a.spoonacularScore - b.spoonacularScore;
+      } else {
+        var nameA = a.title.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.title.toUpperCase(); // ignore upper and lowercase
+        if (nameA > nameB) {
+          return -1;
+        }
+        if (nameA < nameB) {
+          return 1;
+        }
+        // names must be equal
+        return 0;
+      }
+    });
   } else if (byOrder === "orderSmaScore") {
-    array = array.sort((b, a) => b.spoonacularScore - a.spoonacularScore);
+    array = array.sort((b, a) => {
+      if (b.spoonacularScore - a.spoonacularScore !== 0) {
+        return b.spoonacularScore - a.spoonacularScore;
+      } else {
+        var nameA = a.title.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.title.toUpperCase(); // ignore upper and lowercase
+        if (nameA > nameB) {
+          return -1;
+        }
+        if (nameA < nameB) {
+          return 1;
+        }
+        // names must be equal
+        return 0;
+      }
+    });
   }
 
   return array;
