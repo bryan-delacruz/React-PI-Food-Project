bfc87f9588bf663bfb2475a5b95586616a4d3a90
Bryan De La Cruz <bryan.delacruza@gmail.com>
2022-01-06 07:55:38 -0500

    Refactor FormCreateRecipeDiets

--- client/src/Components/FormCreateRecipeDiets/FormCreateRecipeDiets.jsx
+++ client/src/Components/FormCreateRecipeDiets/FormCreateRecipeDiets.jsx
@@ -19,26 +19,25 @@ const FormCreateRecipeDiets = ({ form, setForm, style }) => {
     <div>
       <div className={style.dieta_filter}>
         <p className={style.dieta_title}>Diets</p>
-        {diets.length > 0
-          ? diets.map((e, i) => {
-              let prop = e.replaceAll(" ", "_");
-              return (
-                <div key={i} className={style.flex_filter}>
-                  <input
-                    className={style.input}
-                    id={e}
-                    type="checkbox"
-                    name={prop}
-                    value={e}
-                    onChange={(e) => handleInputCheckBox(e)}
-                  />
-                  <label className={style.label} htmlFor={e}>
-                    {e}
-                  </label>
-                </div>
-              );
-            })
-          : null}
+        {diets.length > 0 &&
+          diets.map((e, i) => {
+            let prop = e.replaceAll(" ", "_");
+            return (
+              <div key={i} className={style.flex_filter}>
+                <input
+                  className={style.input}
+                  id={e}
+                  type="checkbox"
+                  name={prop}
+                  value={e}
+                  onChange={(e) => handleInputCheckBox(e)}
+                />
+                <label className={style.label} htmlFor={e}>
+                  {e}
+                </label>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
